bb9b9441e927afcca335e7cec20e90b8fee42a98
Dipali <dipssonawane123@gmail.com>
2022-06-30 02:47:15 +0530

    modified:Adding at start function is modified

---
+++ src/com/datastructure/Main.java
@@ -0,0 +1,12 @@
+/*Ability to create Linked List by adding 30 and 56   to 70
+        - Node with data 70 is First Created
+        - Next 30 is added to 70
+        - Finally 56 is added to 30
+        - LinkedList Sequence: 56->30->70*/
+package com.datastructure;
+public class Main {
+    public static void main(String[] args){System.out.println("Welcome to linkedList DataStructure Program");
+        Operation.addDataAtStart();
+
+    }
+}
