8f1e3af93302ae6fcec2ef4a6ebf195db8d1b478
Dipali <dipssonawane123@gmail.com>
2022-06-30 03:13:59 +0530

    Modified :Insert in between method feature is modified

--- src/com/datastructure/Operation.java
+++ src/com/datastructure/Operation.java
@@ -17,5 +17,19 @@ public class Operation {
         linkedList.append(30);
         linkedList.append(70);
         linkedList.print();
+
+    }
+    //created method for insert in between
+    public static void insertInBetween(){
+        LinkedList linkedList=new LinkedList();
+        Node secondNode =linkedList.push(70);
+        Node firstNode =linkedList.push(56);
+        Node newNode =new Node(30);
+        System.out.println("Before");
+        linkedList.print();
+        linkedList.insertInBetween(firstNode,newNode);
+        System.out.println(                             );
+        System.out.println("After");
+        linkedList.print();
     }
 }
